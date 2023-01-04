fb2cc9e96621947c8da777ef484a1be9f347553a
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2023-01-04 22:07:37 +0100

    start

---
+++ script.js
@@ -0,0 +1,4 @@
+const burgerBTn = document.querySelector('.burger')
+const barIco = document.querySelector('.fa-bars')
+const xIco = document.querySelector('.fa-times')
+const nav = document.querySelector('nav ul')
