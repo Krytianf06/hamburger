17750afde99ba0dba35f876997335aef4c51cc43
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2023-01-05 19:03:59 +0100

    Koniec

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const burgerBTn = document.querySelector('.burger')
-const barIco = document.querySelector('.fa-bars')
-const xIco = document.querySelector('.fa-times')
-const nav = document.querySelector('nav ul')
+const burgerBtn = document.querySelector(".burger");
+const barIco = document.querySelector(".fa-bars");
+const xIco = document.querySelector(".fa-times");
+const nav = document.querySelector("nav ul");
+
+const hanleNav = () => {
+	nav.classList.toggle("active");
+	burgerBtn.classList.toggle("active");
+	barIco.classList.toggle("hide");
+	xIco.classList.toggle("hide");
+};
+
+burgerBtn.addEventListener("click", hanleNav);
